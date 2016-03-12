252d9b4a4d93bbb77c7b8100c7a3547858787a76
ronathan <ronaldvictorino@gmail.com>
2016-03-12 18:08:48 -0500

    Error message showing.

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,27 @@
 $(function(){
 
-	$("#testForm").submit(function ( e ) {
-
+	$("#testForm").submit(function (e) {
 		e.preventDefault();
 
-		var firstName = $("#firstname").val();
+		var firstName = $("#firstName").val();
 		if (firstName == null || firstName.length <= 0) {
+			showErrors("First Name");
+			return;	
+		}
+
+		var lastName = $("#lastName").val();
+		if (lastName == null || lastName.length <= 0) {
+			$('#errorMessageBox').show();
+			$('#missingVariable').text("Last Name");
 			return;
-		} 
+		}
+
+
 	});
+
+	function showErrors(errorMessage) {
+		$('#errorMessageBox').show();
+		$('#missingVariable').text(errorMessage);
+	}
 
 });
